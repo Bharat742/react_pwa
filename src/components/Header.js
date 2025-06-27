2a4b4002842730aa9947d4d89153965a9480dc57
Bharat742 <bharatlal.kumar@technians.com>
2025-06-27 10:48:16 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 // src/Components/Header.js
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
