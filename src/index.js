2947e970c27c6e4690203cb7bfe153a32c53b38d
Bharat742 <bharatlal.kumar@technians.com>
2025-07-03 18:17:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ This is required
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+import App from './App'; 
 
 // import reportWebVitals from './reportWebVitals';
 // by default it might be "unregister()"
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import swDev from "./swDev";
+Notification.requestPermission().then(permission => {
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
